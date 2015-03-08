6b3878c400c12453684be9d910f8ee84765ceab1
DacexiGit <14m4w3s0m31937>
2015-03-08 04:13:30 +0100

    1.1

---
+++ Assets/Scripts/AxeScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var DoChop : int;
+
+function Start () {
+	GetComponent.<Animation>().Stop();
+	DoChop = 1;
+}
+
+function Update () {
+	Chop();
+}
+
+function Chop () {
+	if (DoChop == 1) {
+		if (Input.GetButton ("Fire1")) {
+		    GetComponent.<Animation>().CrossFade("Chop");
+		    DoChop = 0;
+		    yield WaitForSeconds(0.4);
+		    DoChop = 1;
+		}
+	}
+}
